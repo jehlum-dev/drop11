6e7c05fb5ac4c82f386d0c9d70f246b8e2946d46
Talib Hassan Mir <fanaticmirza@gmail.com>
2026-07-07 00:06:51 +0530

    Add GitHub Actions workflow for deploying to GitHub Pages; configure build and deployment steps

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jehlum-dev.github.io',
+  base: '/drop11',
   vite: {
     plugins: [tailwindcss()]
   }
